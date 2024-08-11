831c199da2c8ce91e9ad0b61512275377e090ee6
reharsh <reharshmeena@gmail.com>
2024-08-12 04:41:21 +0530

    new4

--- client/components/main/Skills.tsx
+++ client/components/main/Skills.tsx
@@ -6,7 +6,7 @@ import {
   Skill_data,
 } from "@/constants";
 import React from "react";
-import SkillDataProvider from "../sub/SkillDataProvider";
+// import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
 
 const Skills = () => {
